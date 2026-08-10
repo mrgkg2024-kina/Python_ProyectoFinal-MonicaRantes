536cc3e5ce629026bee15cbef666d79c44f69162
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-09 19:48:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import numpy as np
+import libreria_funciones as lf
 
+st.session_state
+st.set_page_config(page_title="Proyecto Final - Caso Nro. 1", layout="centered")
+st.title("Proyecto Final - Aplicación en Streamlit")
+st.sidebar.title("Menú")
