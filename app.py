9be602efcbcac9758321c6541ffe644b40a4325b
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-09 20:01:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import numpy as np
 from textwrap import dedent
 
 
-st.set_page_config(page_title="Proyecto Final-Caso Nro. 1", layout="centered")
-st.title("Proyecto Final Caso Nro.1 - Streamlit")
+st.set_page_config(page_title="Proyecto Final- Caso Nro. 1", layout="centered")
+st.title("Proyecto Final - Caso de estudio Nro. 1")
 st.sidebar.title("Menú")
