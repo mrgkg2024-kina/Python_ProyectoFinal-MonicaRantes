37397a6e6d12f32fd229bc1e75e88cbc5968d3f1
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-09 19:57:00 -0500

    Update title for Streamlit application

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import numpy as np
 
 st.session_state
 st.set_page_config(page_title="Proyecto Final - Caso Nro. 1", layout="centered")
-st.title("Proyecto Final - Aplicación en Streamlit")
+st.title("Proyecto Final - Caso Nro.1 - Streamlit")
 st.sidebar.title("Menú")
