0db08991fffa115bf7665e42631b23f6cb260c32
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-09 20:02:10 -0500

    Update title formatting in app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from textwrap import dedent
 
 
 st.set_page_config(page_title="Proyecto Final- Caso Nro. 1", layout="centered")
-st.title("Proyecto Final - Caso de estudio Nro. 1")
+st.title("Proyecto Final - Caso de estudio #1")
 st.sidebar.title("Menú")
