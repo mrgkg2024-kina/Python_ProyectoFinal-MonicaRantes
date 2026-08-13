dbfd7ef2f12864ffef96b16b6198d8cb497cc7a4
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-12 20:56:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ from io import BytesIO
 
 from textwrap import dedent
 
+st.set_page_config(
+    page_title="Proyecto Final - Caso Nro. 1",
+    page_icon="📊",
+    layout="wide",                    # usa todo el ancho de la ventana
+    initial_sidebar_state="expanded"  # "auto", "expanded" o "collapsed"
+)
 
-st.set_page_config(page_title="Proyecto Final- Caso Nro. 1", layout="centered")
+#st.set_page_config(page_title="Proyecto Final- Caso Nro. 1", layout="centered")
 st.title("Proyecto Final - Caso de estudio #1")
 st.sidebar.title("Menú")
 
