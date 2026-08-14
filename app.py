df7ad25cb84a1d919493cc394a686ed244b7b5ff
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-13 20:19:10 -0500

    Fix indentation in app.py for dataframe display

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ else:
 
         col1, col2 = st.columns(2)
             with col1:
-                 st.dataframe(raw_data.info())
+                st.dataframe(raw_data.info())
         
 
     
