d00c3e0e3b49d57a7ddf0ad102374d8ce7e5d3ec
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-15 13:45:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -148,8 +148,8 @@ else:
             s = buf.getvalue()
             st.markdown(f"```text\n{s}\n```")  # o st.text(s) / st.markdown(f"```text\n{s}\n```")
         with col2:
-            st.markdown('<p style="color:#2b8cbe; font-weight:bold; text-align:center; font-size:18px;"> Conteo de valores nulos</p>', unsafe_allow_html=True)
-            st.dataframe(df.isna().sum().to_frame("null_count"), width=250)            
+            st.markdown('<p style="color:#2b8cbe; font-weight:bold; font-size:18px;"> Conteo de valores nulos</p>', unsafe_allow_html=True)
+            st.dataframe(df.isna().sum().to_frame("null_count"), width=300)            
         
 
     
