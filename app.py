316b5806cfe3011a093c3f4f42b70126d1d8e9d9
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-16 14:39:51 -0500

    Adjust column layout and dataframe width for stats
    Updated column layout and dataframe display width for numeric statistics.

--- app.py
+++ app.py
@@ -223,13 +223,13 @@ else:
         cat_cols = df.select_dtypes(include=["object", "category", "string"]).columns
 
         # Estadísticas de variables numéricas
-        col1, col2 = st.columns([1, 2])
+        col1, col2 = st.columns(2)
         
         with col1:
             st.markdown('<p style="color:#2b8cbe; font-weight:bold; text-align:center; font-size:18px;"> Estadísticas para variables numéricas</p>', 
                         unsafe_allow_html=True)
             estadisticas_num = df.describe()
-            st.dataframe(estadisticas_num) 
+            st.dataframe(estadisticas_num, width=300) 
 
         with col2:
            # Interpretación simple para numéricas
