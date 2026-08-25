5f55cb18a387a17cec761f341b53d24989e7b0e2
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-24 21:20:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1174,7 +1174,7 @@ else:
                 }
             
                 div[data-testid="stMetricLabel"] p {
-                    color: #FFFFFF;       /* Color del título */
+                    color: #000000;       /* Color del título */
                     font-size: 14px;      /* Tamaño del título */
                     font-weight: 600;
                     text-align: center;
