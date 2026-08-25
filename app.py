de919c3a2f38ffe3ad630f71b128454eaadbe3c2
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-24 21:56:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1284,11 +1284,12 @@ else:
                         variables_numericas
                     )
             
-                    fig, ax = plt.subplots(figsize=(4, 2))
+                    fig, ax = plt.subplots(figsize=(5, 3))
             
                     sns.histplot(
                         data=df,
                         x=variable,
+                        bins="auto",
                         kde=True,
                         color="steelblue",
                         ax=ax
