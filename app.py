685a2429440a654182ab84cfd2ba27700d8433da
mrgkg2024-kina <mrgkg2024@gmail.com>
2026-08-24 20:28:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1049,15 +1049,15 @@ else:
                         vmin=-1,
                         vmax=1,
                         linewidths=0.3,
-                        annot_kws={"fontsize": 4},
-                        cbar_kws={"shrink": 0.4},
+                        annot_kws={"fontsize": 3},
+                        cbar_kws={"shrink": 0.5},
                         ax=ax
                     )
                 
                     ax.set_title("Matriz de correlación", fontsize=6,  pad=7)
-                    ax.tick_params(axis="x", labelsize=4, rotation=45)
+                    ax.tick_params(axis="x", labelsize=5, rotation=45)
 
-                    ax.tick_params(axis="y", labelsize=4, rotation=0)
+                    ax.tick_params(axis="y", labelsize=5, rotation=0)
                 
                     plt.tight_layout()
                     st.pyplot(fig)
